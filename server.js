d03f7e52be839e1bad6a51514910818c12eb351e
matheus.leal <matheus.leal@corp.globo.com>
2019-05-29 16:53:34 -0300

    create /transactions/new

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 var express = require('express');
 var app = express();
 
+// Handling multipart/form-data from /transactions/new
+const multer = require('multer');
+const upload = multer();
+
+app.use(express.static(__dirname+'/view'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname+'/view/routes/dashboard.html');
 });
 
 app.get('/transactions/list', function (req, res) {
-  res.send('List Transactions from one user');
+  res.sendFile(__dirname+'/view/routes/list.html');
 });
 
 app.get('/transactions/create', function (req, res) {
-  res.send('Create a transaction');
+  res.sendFile(__dirname+'/view/routes/create.html');
+});
+
+app.post('/transactions/new', upload.any(), function (req, res) {
+  console.log(req.body);
+  console.log(req.files);
+  res.redirect('/transactions/create')
 });
 
 app.listen(3000, function () {
-  console.log('Listening on port localhost:3000');
+  console.log('Listening on port http://localhost:3000');
 });
