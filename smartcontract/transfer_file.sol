d12fe6c0d13deda37de768d5c9b38725c16c2754
matheus.leal <matheus.leal@corp.globo.com>
2019-06-07 08:49:11 -0300

    smart contract update

--- smartcontract/transfer_file.sol
+++ smartcontract/transfer_file.sol
@@ -5,11 +5,12 @@ contract TransferFile {
     mapping (string => bool) private transaction;
     mapping (string => string) private data;
 
-    function setTransaction(string memory data_to_send, string memory receiver) public payable{
+    function setTransaction(string memory data_to_send, string memory receiver) public payable returns (string memory){
         string memory transaction_sender = toString(msg.sender);
         string memory transaction_id = string(abi.encodePacked(transaction_sender,receiver,Time_call()));
         transaction[transaction_id] = true;
         data[transaction_id] = data_to_send;
+        return transaction_id;
     }
 
     function getFile(string memory tran_id) public returns (string memory){
