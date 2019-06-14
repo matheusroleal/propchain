4b368d6689402805834263edf5f23dca73cb8769
matheus.leal <matheus.leal@corp.globo.com>
2019-06-14 08:14:16 -0300

    exporting fuctions

--- utils/file_system.js
+++ utils/file_system.js
@@ -1,18 +1,17 @@
 var fs = require('fs');
 
-module.exports = {
-  load_buffer_file : function (buffer, fileName) {
-    var filePath = './'+fileName
-    var wstream = fs.createWriteStream(filePath);
+exports.load_buffer_file = function (buffer, fileName) {
+  var filePath = './'+fileName
+  var wstream = fs.createWriteStream(filePath);
 
-    wstream.write(buffer);
+  wstream.write(buffer);
 
-    wstream.end();
-    return filePath;
-  },
-  remove_file : function (filePath){
-    fs.unlinkSync(filePath)
+  wstream.end();
+  return filePath;
+}
 
-    console.log('File '+ filePath +' deleted!');
-  }
+exports.remove_file = function (filePath){
+  fs.unlinkSync(filePath)
+
+  console.log('File '+ filePath +' deleted!');
 }
