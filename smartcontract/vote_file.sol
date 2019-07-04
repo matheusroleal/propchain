0932f1cdd6dcca7c436ef48c6259ecee08f93a75
matheus.leal <matheus.leal@corp.globo.com>
2019-07-04 14:02:28 -0300

    smartcontract update

--- smartcontract/vote_file.sol
+++ smartcontract/vote_file.sol
@@ -16,6 +16,7 @@ contract voteTransfer {
     // Data related with defining winner
     struct Proposal {
         string file_name;
+        string file_id;
         address sender;
     }
 
@@ -27,27 +28,28 @@ contract voteTransfer {
     }
 
     // Defining a file transaction to the server
-    function setProposal(string memory data_to_send) public payable {
+    function setProposal(string memory data_id, string memory data_name) public payable {
         if ( toProposal >= proposals.length) return;
         string memory transaction_sender = toString(msg.sender);
-        transaction[data_to_send] = true;
+        transaction[data_id] = true;
         // Add ownership and initialize the vote counter
-        data[data_to_send] = transaction_sender;
-        vote[data_to_send] = 0;
+        data[data_id] = transaction_sender;
+        vote[data_id] = 0;
         // Add proposal to the proposals array
-        proposals[toProposal].file_name = data_to_send;
+        proposals[toProposal].file_name = data_name;
+        proposals[toProposal].file_id = data_id;
         proposals[toProposal].sender = msg.sender;
         toProposal += 1;
     }
 
     // Voting for a file
-    function voteFile(string memory data_to_send) public payable {
+    function voteFile(string memory data_id) public payable {
         if ( voteTransaction[msg.sender] == true ) return;
         string memory transaction_sender = toString(msg.sender);
         // Set the transaction id and mark as voted
-        string memory transaction_id = string(abi.encodePacked(transaction_sender,data_to_send));
+        string memory transaction_id = string(abi.encodePacked(transaction_sender,data_id));
         voteTransaction[msg.sender] = true;
-        vote[data_to_send] = vote[data_to_send] + 1 + winValue[msg.sender];
+        vote[data_id] = vote[data_id] + 1 + winValue[msg.sender];
     }
 
     // Show the proposals defined
@@ -64,19 +66,20 @@ contract voteTransfer {
     }
 
     // Defining most voted file
-    function winningProposal() public payable returns (string memory _winningProposal) {
+    function winningProposal() public payable returns (string memory _winningProposal, address _winningAddress) {
         uint256 winningVoteCount = 0;
         address winningAdd;
         for (uint8 prop = 0; prop < proposals.length; prop++){
-            string memory fn = proposals[prop].file_name;
+            string memory fn = proposals[prop].file_id;
             if (vote[fn] > winningVoteCount) {
                 winningVoteCount = vote[fn];
                 winningAdd = proposals[prop].sender;
                 _winningProposal = fn;
+                _winningAddress = winningAdd;
             }
         }
         winValue[winningAdd] += 2;
-        return _winningProposal;
+        return (_winningProposal, _winningAddress);
     }
 
     // Utils functions
